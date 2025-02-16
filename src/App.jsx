fcd85c97a1726546ff86a6e04a818ebf4c575b5e
Steve Gibbs <swaygibb@gmail.com>
2025-02-16 10:08:03 -0600

    add Posts page and PostDetail component; update Dashboard with navigation to Posts

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Reports from "./pages/Reports";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import PostDetail from "./pages/PostDetail";
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/reports" element={<Reports />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:postId" element={<PostDetail />} />
       </Routes>
     </Router>
   );
